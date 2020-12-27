93978e851e0b66c912cce3eba79375bd57d50873
royce <roycedcano@gmail.com>
2020-12-27 03:30:11 -0800

    edit

--- setup17/script_bootloader/usr/dnsmasq.d/etc/monit.d/dnsmasq.d
+++
@@ -1,4 +0,0 @@
-check program DNSMASQ.D with path "/opt/script_bootloader/usr/dnsmasq.d/bin/dnsmasq.d_enable.service" every 6 cycles
-    start program = "/opt/script_bootloader/usr/dnsmasq.d/bin/dnsmasq.d_enable.service"
-    stop program = "/opt/script_bootloader/usr/dnsmasq.d/bin/dnsmasq.d_disable.service"
-    if status == 1 then alert
